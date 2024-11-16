585aa5c9c6fdfb2d7c3786a07c030e8bed336b91
Alexandru Zvinca <alexandruzvinca@yahoo.com>
2024-11-16 14:23:27 +0200

    Worked at tabs

--- FE_Expo/cultureit/app/(tabs)/_layout.jsx
+++ FE_Expo/cultureit/app/(tabs)/_layout.jsx
@@ -1,14 +1,20 @@
-import { View, Image } from 'react-native'
+import { View, Text, Image, BackHandler } from 'react-native'
 import React from 'react'
 import {Tabs} from 'expo-router'
 import { icons } from '../../constants'
 
-const TabIcon = ({icon}) => {
+const TabIcon = ({icon, color, name, focused}) => {
   return (
-    <View>
+    <View className="items-center justify-center">
       <Image
         source = {icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
       />
+      <Text className = {`${focused ? 'font-Inter-Bold' : 'font-Inter-Regular'} text-xs`}>
+      {name}
+      </Text>
     </View>
   )
 }
@@ -16,22 +22,80 @@ const TabIcon = ({icon}) => {
 const TabsLayout = () => {
   return (
    <>
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: '#F7BA4B',
+        tabBarInactiveTintColor: '#CDCDE0',
+        tabBarStyle: {
+          backgroundColor: "#F3E9B5"
+        }
+      }}
+    >
+      
       <Tabs.Screen
-        name = "map"
-        options = {{
-          title: 'Map',
-          headerShown: false,
-          tabBarIcon: ({color, focused}) => ( 
-            <TabIcon
-              icon = {icons.map}
-              color = {color}
-              name = "Map"
-              focused={focused}
+              name = "map"
+              options = {{
+                title: 'Map',
+                headerShown: false,
+                tabBarIcon: ({color, focused}) => ( 
+                  <TabIcon
+                    icon = {icons.map}
+                    color = {color}
+                    name = "Map"
+                    focused={focused}
+                  />
+                )
+              }}
             />
-          )
-        }}
+              
+      <Tabs.Screen
+              name = "trending"
+              options = {{
+                title: 'Trending',
+                headerShown: false,
+                tabBarIcon: ({color, focused}) => ( 
+                  <TabIcon
+                    icon = {icons.trending}
+                    color = {color}
+                    name = "Trending"
+                    focused={focused}
+                  />
+                )
+              }}
       />
+
+      <Tabs.Screen
+              name = "favorites"
+              options = {{
+                title: 'Favorites',
+                headerShown: false,
+                tabBarIcon: ({color, focused}) => ( 
+                  <TabIcon
+                    icon = {icons.favorites}
+                    color = {color}
+                    name = "Favorites"
+                    focused={focused}
+                  />
+                )
+              }}
+            />
+
+      <Tabs.Screen
+              name = "profile"
+              options = {{
+                title: 'Profile',
+                headerShown: false,
+                tabBarIcon: ({color, focused}) => ( 
+                  <TabIcon
+                    icon = {icons.profile}
+                    color = {color}
+                    name = "Profile"
+                    focused={focused}
+                  />
+                )
+              }}
+            />
     </Tabs>
    </>
   )
--- FE_Expo/cultureit/constants/icons.js
+++ FE_Expo/cultureit/constants/icons.js
@@ -1,5 +1,11 @@
 import map from "../assets/icons/map.png"
+import trending from "../assets/icons/trending.png"
+import favorites from "../assets/icons/favorites.png"
+import profile from "../assets/icons/profile.png"
 
 export default {
-    map
+    map,
+    trending,
+    favorites,
+    profile
 };
